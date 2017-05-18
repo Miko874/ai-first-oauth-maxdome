78bc995f00d801fa870f9827db9a5016fdadb6c6
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-18 17:09:56 +0200

    Use dmiddlewares

--- src/web.js
+++ src/web.js
@@ -4,10 +4,12 @@ const maxdome = require('drequest-maxdome').getRequestBuilder();
 const redis = require('dredis')(process.env.REDIS_URL);
 
 const app = require('dexpress')();
-app.use(require('express').static('www'));
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+require('dmiddlewares')(app, [
+  require('express').static('www'),
+  bodyParser.json(),
+  bodyParser.urlencoded({ extended: false }),
+]);
 require('dcontrollers')(app, [
   require('./controllers/linkedAccount')({ maxdome, redis }),
   require('./controllers/signin')({ maxdome, redis }),
