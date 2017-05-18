23d7fb6bf3cc89c20ff2f5f28487ea1a0df5db93
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-18 17:16:42 +0200

    Formatting

--- src/web.js
+++ src/web.js
@@ -1,15 +1,18 @@
 require('dotenv-safe').config();
 
-const maxdome = require('drequest-maxdome').getRequestBuilder();
-const redis = require('dredis')(process.env.REDIS_URL);
-
 const app = require('dexpress')();
+
 const bodyParser = require('body-parser');
+
 require('dmiddlewares')(app, [
   require('express').static('www'),
   bodyParser.json(),
   bodyParser.urlencoded({ extended: false }),
 ]);
+
+const maxdome = require('drequest-maxdome').getRequestBuilder();
+const redis = require('dredis')(process.env.REDIS_URL);
+
 require('dcontrollers')(app, [
   require('./controllers/linkedAccount')({ maxdome, redis }),
   require('./controllers/signin')({ maxdome, redis }),
